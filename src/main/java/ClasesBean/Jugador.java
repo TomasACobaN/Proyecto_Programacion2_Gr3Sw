09e07261163ce08beaa904cdd2f20bef14d7be60
Matias <jhony.espinoza@epn.edu.ec>
2025-07-30 20:58:58 -0500

    Cambio de nombres

--- src/main/java/ClasesBean/Jugador.java
+++ src/main/java/ClasesBean/Jugador.java
@@ -4,29 +4,54 @@
  */
 package ClasesBean;
 
+/**
+ *
+ * @author tcoba
+ */
+public class Jugador extends Persona {            
+    private String nickname;
+    private String pais;
+    private int ranking;
+    private int victorias;
 
-public class Jugador {
-    private String nombre;
-    private int edad;
-
-    public Jugador(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
+    public Jugador(String nombre, int edad, String cedula) {
+        super( edad, nombre);
+    }
+    
+    public Jugador( int edad,String nombre, String nickname, String pais, int ranking, int victorias) {
+        super( edad, nombre);
+        this.nickname = nickname;
+        this.pais = pais;
+        this.ranking = ranking;
+        this.victorias=victorias;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getNickname() {
+        return nickname;
     }
 
-    public int getEdad() {
-        return edad;
+    public String getPais() {
+        return pais;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public int getRanking() {
+        return ranking;
+    }
+    public int getVictorias(){
+        return victorias;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+    public void setVictorias(int victorias){
+        this.victorias=victorias;
     }
 }
